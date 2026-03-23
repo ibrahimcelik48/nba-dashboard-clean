9277ea6054a0c8a1e1151b43f8415df56e159484
İbrahim Çelik <ibrahimcelik@Ibrahim-MacBook-Air.local>
2026-03-23 21:57:18 +0300

    add scoreboard api

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [games, setGames] = useState([]);
 
   useEffect(() => {
-    fetch("https://nba-dashboard-backend.onrender.com/api/games/next")
+    fetch("https://nba-dashboard-backend.onrender.com/api/games")
       .then((res) => res.json())
       .then((data) => {
-        console.log("GELEN DATA:", data);
-
-        // 🔥 KRİTİK SATIR
-        setGames(data.upcoming || []);
+        console.log(data);
+        setGames(data.games || []);
       })
-      .catch((err) => {
-        console.error(err);
-      });
+      .catch((err) => console.error(err));
   }, []);
 
   return (
-    <div style={{ textAlign: "center", color: "white" }}>
+    <div className="app">
       <h1>NBA Dashboard</h1>
 
-      <h2>Yaklaşan Maçlar</h2>
+      <div className="container">
+        {games.map((game) => (
+          <div className="card" key={game.id}>
+            <div className="inner">
 
-      {games.length === 0 ? (
-        <p>Maç bulunamadı</p>
-      ) : (
-        <div>
-          {games.map((team) => (
-            <div key={team.id} style={{ margin: "20px" }}>
-              <img src={team.logo} width="80" />
-              <h3>{team.name}</h3>
+              {/* FRONT */}
+              <div className="front">
+                <img src={game.homeLogo} />
+                <h3>{game.home}</h3>
+
+                <span>VS</span>
+
+                <img src={game.awayLogo} />
+                <h3>{game.away}</h3>
+              </div>
+
+              {/* BACK */}
+              <div className="back">
+                <h2>{game.homeScore} - {game.awayScore}</h2>
+                <p>{game.date}</p>
+              </div>
+
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
